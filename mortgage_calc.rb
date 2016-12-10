239004d3ec6ddf4a52fe223f070f3e85540984e3
Dalibor Spok <dalspok@gmail.com>
2016-12-10 13:49:49 +0100

    new calc

--- mortgage_calc.rb
+++ mortgage_calc.rb
@@ -1,20 +1,26 @@
 
+def valid_number?(num)
+  num.to_f.to_s == num || num.to_i.to_s == num
+end
+
 def get_data(text)
   loop do
     print text
-    input = gets.chomp.to_f
-    return input if input >= 0
-    puts "Please enter number only, greater than zero. "
+    input = gets.chomp.strip
+    return input.to_f if valid_number?(input) && input.to_f >= 0
+    puts "Please enter non-negative number."
   end
 end
 
-puts "Welcome to mortgage calculator"
+puts "\nWelcome to mortgage calculator\n\n"
 
 loop do
+  # input
   amount = get_data("Loan amount: ")
   perc_year = get_data("Percentage (per year, in %): ")
   dur_years = get_data("Duration (years): ")
 
+  # calculation
   dur_months = dur_years * 12
   perc_month = (perc_year / 100.0) / 12.0
   payment = amount * (perc_month / (1 - (1 + perc_month)**-dur_months))
@@ -22,7 +28,10 @@ loop do
   puts "-------------------------------------------"
   puts "Your monthly payment is #{payment.round(2)}"
   puts "-------------------------------------------"
-  
+
+  # another?
   print "Another calucation? (y/n): "
-  break if gets.chomp.downcase.start_with?("n")
+  break unless gets.chomp.strip.downcase.start_with?("y")
 end
+
+puts "Thank you for using our calculator."
