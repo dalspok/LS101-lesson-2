9b566f0f73c84f131d6cb1de5fcf2b5c41b52018
Dalibor Spok <dalspok@gmail.com>
2017-03-04 10:54:28 +0100

    calc2

--- calculator_i2.rb
+++ calculator_i2.rb
@@ -1,5 +1,5 @@
 def prompt(message)
-  puts ">> #{message}"  
+  puts ">> #{message}"
 end
 
 def valid_number(number)
@@ -8,21 +8,22 @@ end
 
 def operation_to_msg(operation)
   case operation
-    when 1
-      "Adding"
-    when 2
-      "Subtracting"
-    when 3
-      "Multiplying"
-    when 4
-      "Dividing"
+  when 1
+    "Adding"
+  when 2
+    "Subtracting"
+  when 3
+    "Multiplying"
+  when 4
+    "Dividing"
   end
 end
 
 prompt "Welcome to calculator"
-loop do 
+
+loop do
   number1, number2, operation = nil
-  
+
   loop do
     prompt "What's the first number?"
     number1 = gets.chomp
@@ -38,35 +39,36 @@ loop do
   end
 
   prompt <<-Q
-    What operation? 
-    1)add 
-    2)subtract 
-    3)multiply 
+    What operation?
+    1)add
+    2)subtract
+    3)multiply
     4)divide
   Q
 
-  loop do  
+  loop do
     operation = gets.chomp.to_i
-    break if [1,2,3,4].include?(operation)
+    break if [1, 2, 3, 4].include?(operation)
     prompt "Please enter valid operation"
   end
 
   prompt "#{operation_to_msg(operation)} numbers..."
 
   result = case operation
-            when 1
-              number1.to_i + number2.to_i 
-            when 2
-              number1.to_i - number2.to_i 
-            when 3
-              number1.to_i * number2.to_i 
-            when 4
-              number1.to_i / number2.to_i.to_f
-          end
+           when 1
+             number1.to_i + number2.to_i
+           when 2
+             number1.to_i - number2.to_i
+           when 3
+             number1.to_i * number2.to_i
+           when 4
+             number1.to_i / number2.to_i.to_f
+           end
 
   puts "Result is #{result}"
   prompt "Do you want to continue? (y-yes)"
   continue = gets.chomp
   break if continue != "y"
 end
+
 puts "Thank you for using calculator"
